71586670a8fe633fd702778eae07622737b5ff90
michelebenveuto <35434145+michelebenveuto@users.noreply.github.com>
2019-01-27 16:45:10 -0600

    Se logra hacer calculos
    La calculadora ya puede hacer calculos desde un documento tipo txt externo

---
+++ src/src/Main.java
@@ -0,0 +1,66 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MyCalculator calculator = new MyCalculator();
+        StackArrayList<Integer> stack = new StackArrayList<Integer>();
+        ArrayList operation = new ArrayList();
+        int operator = 0;
+        int operand = 0;
+        try {
+            Stream<String> lines = Files.lines(
+                    Paths.get("C:\\Users\\HP\\Desktop\\datos.txt"),
+                    StandardCharsets.UTF_8
+            );
+            lines.forEach(s ->{
+                    operation.add(s);
+            });
+        }catch (IOException exception){
+            System.out.println("Error");
+        }
+
+
+        String operationToDo= (String) operation.get(0);
+        for (int i =0; i<operationToDo.length();i++){
+            String currentString=operationToDo.substring(i,i+1);
+
+            try {
+                int currentNumber = Integer.parseInt(currentString);
+                stack.push(currentNumber);
+            }catch(NumberFormatException nfe){
+                if (!currentString.equals(" ")){
+                    if (currentString.equals("+")){
+                        operand=stack.pop();
+                        operator= stack.pop();
+                        stack.push(calculator.calculate(operand,operator,"+"));
+                    }
+                    else if (currentString.equals("*")){
+                        operand=stack.pop();
+                        operator=stack.pop();
+                        stack.push(calculator.calculate(operand,operator,"*"));
+                    }
+                    else if (currentString.equals("-")){
+                        operator=stack.pop();
+                        operand= stack.pop();
+                        stack.push(calculator.calculate(operand,operator,"-"));
+                    }
+                    else if (currentString.equals("/")){
+                        operator=stack.pop();
+                        operand= stack.pop();
+                        stack.push(calculator.calculate(operand,operator,"/"));
+                    }
+                }
+            }
+        }
+        System.out.println(stack.peek());
+    }
+}
--- src/src/MyCalculator.java
+++ src/src/MyCalculator.java
@@ -8,18 +8,18 @@ Clase MyCalculator.java implente la interfaz Calculator
 public class MyCalculator implements Calculator{
 
     public int calculate(int num1, int num2, String op){
-    	int retorno;
+    	int retorno=0;
     	if (op=="+"){
-    		retorno == num1+num2;
+    		retorno = num1+num2;
     	}
     	else if(op=="-"){
-    		retorno == num1-num2;
+    		retorno = num1-num2;
     	}
     	else if(op=="*"){
-    		retorno == num1*num2;
+    		retorno = num1*num2;
     	}
     	else if(op=="/"){
-    		retorno == num1/num2;
+    		retorno = num1/num2;
     	}
     	return retorno;
     }
